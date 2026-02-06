df9084a268d5e2ff3c2368b9f6bc5142879d5c18
Safwan Sadid <safwan55.ah@gmail.com>
2026-02-06 20:55:18 +0600

    Initialize AppsScreen with BlockedAppsBloc to load blocked apps on startup

--- lib/frontend/apps.dart
+++ lib/frontend/apps.dart
@@ -1,5 +1,8 @@
+import 'package:digital_itikaf/backend/bloc/blocked_apps/bloc.dart';
+import 'package:digital_itikaf/backend/bloc/blocked_apps/blocked_apps_events.dart';
 import 'package:digital_itikaf/util/Theme/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppsScreen extends StatefulWidget {
   const AppsScreen({super.key});
@@ -9,6 +12,13 @@ class AppsScreen extends StatefulWidget {
 }
 
 class _AppsScreenState extends State<AppsScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    context.read<BlockedAppsBloc>().add(LoadBlockedApps());
+  }
+
   @override
   Widget build(BuildContext context) {
     final isDark = Theme.of(context).brightness == Brightness.dark;
